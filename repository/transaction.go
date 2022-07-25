61ca1150b3244f383d72f83cb86029d21b174796
Daniil Dulin <daniildulin@gmail.com>
2022-07-25 14:52:12 +0300

    update tx repository

--- repository/transaction.go
+++ repository/transaction.go
@@ -5,14 +5,37 @@ import (
 	"github.com/MinterTeam/explorer-sdk/helpers"
 	"github.com/MinterTeam/explorer-sdk/models"
 	"github.com/uptrace/bun"
+	"strings"
 )
+
+func (rTransaction *TransactionRepository) SaveInvalid(tx *models.InvalidTransaction) (*models.InvalidTransaction, error) {
+	_, err := rTransaction.db.NewInsert().
+		Model(tx).
+		Exec(rTransaction.ctx)
+	return tx, err
+}
+
+func (rTransaction *TransactionRepository) SaveAll(list []models.Transaction) ([]models.Transaction, error) {
+	txs := list
+	_, err := rTransaction.db.NewInsert().
+		Model(&txs).
+		Exec(rTransaction.ctx)
+	return txs, err
+}
+
+func (rTransaction *TransactionRepository) Save(tx *models.Transaction) (*models.Transaction, error) {
+	_, err := rTransaction.db.NewInsert().
+		Model(tx).
+		Exec(rTransaction.ctx)
+	return tx, err
+}
 
 func (rTransaction *TransactionRepository) GetByHash(hash string) (*models.Transaction, error) {
 	tx := new(models.Transaction)
 	err := rTransaction.db.
 		NewSelect().
 		Model(tx).
-		Where("hash = ?", helpers.RemovePrefix(hash)).
+		Where("hash = ?", strings.ToLower(helpers.RemovePrefix(hash))).
 		Scan(rTransaction.ctx)
 	if err != nil {
 		return nil, err
